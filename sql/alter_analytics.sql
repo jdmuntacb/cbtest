6844b870c3d52dc8c17a3d390815440e999718fe
Jagadesh Munta <jagadesh.munta@couchbase.com>
2020-07-29 18:52:49 -0700

    Handling multi-line queries with ; end as if sql

--- sql/alter_analytics.sql
+++ sql/alter_analytics.sql
@@ -1,9 +1,10 @@
 
-ALTER COLLECTION MyBucket.MyScope.MyCollection ENABLE ANALYTICS
-SELECT * FROM MyBucket.MyScope.MyCollection
+ALTER COLLECTION MyBucket.MyScope.MyCollection ENABLE ANALYTICS;
+SELECT * FROM MyBucket.MyScope.MyCollection;
 
 SELECT * FROM Metadata.`Dataverse`;
-SELECT VALUE d.DataverseName || '.' || d.DatasetName FROM Metadata.`Dataset` d WHERE d.DataverseName <> "Metadata"
+SELECT VALUE d.DataverseName || '.' || d.DatasetName FROM Metadata.`Dataset` d 
+  WHERE d.DataverseName <> "Metadata";
 
 
 
--- sql/alter_analytics2.sql
+++ sql/alter_analytics2.sql
@@ -1,17 +1,17 @@
 
-#ALTER COLLECTION MyBucket ENABLE ANALYTICS
-#SELECT * FROM MyBucket
+-- ALTER COLLECTION MyBucket ENABLE ANALYTICS;
+-- SELECT * FROM MyBucket;
 
-ALTER COLLECTION MyBucket.MyScope.MyCollection ENABLE ANALYTICS
-SELECT * FROM MyBucket.MyScope.MyCollection
+ALTER COLLECTION MyBucket.MyScope.MyCollection ENABLE ANALYTICS;
+SELECT * FROM MyBucket.MyScope.MyCollection;
 
 CREATE DATAVERSE MyBucket_MyScope_MyCollection;
 
-CREATE ANALYTICS COLLECTION MyBucket_MyScope_MyCollection.MyNewCollection ON MyBucket.MyScope.MyCollection WHERE `role` = "Manager"
-SELECT * FROM MyBucket_MyScope_MyCollection.MyNewCollection
+CREATE ANALYTICS COLLECTION MyBucket_MyScope_MyCollection.MyNewCollection ON MyBucket.MyScope.MyCollection WHERE `role` = "Manager";
+SELECT * FROM MyBucket_MyScope_MyCollection.MyNewCollection;
 
 SELECT * FROM Metadata.`Dataverse`;
-SELECT VALUE d.DataverseName || '.' || d.DatasetName FROM Metadata.`Dataset` d WHERE d.DataverseName <> "Metadata"
+SELECT VALUE d.DataverseName || '.' || d.DatasetName FROM Metadata.`Dataset` d WHERE d.DataverseName <> "Metadata";
 
 
 
